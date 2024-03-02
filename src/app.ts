7e8bc5d6816f19f2809cf3dae6aa14a8147871ed
khois345 <khois345@gmail.com>
2024-03-01 17:04:31 -0800

    First commit

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import { customAlphabet } from 'nanoid';
 import EVENTS from './config/events';
 import CONFIG from './config/config';
 
+const DEBUG = true;
+
 const app = express();
 const httpServer = createServer(app);
 
@@ -15,6 +17,8 @@ const io = new Server(httpServer, {
   },
   maxHttpBufferSize: 1e8,
 });
+
+const hostIDs = {};
 
 httpServer.listen(CONFIG.PORT, () => {
   console.log(`Server is up and running on port: ${CONFIG.PORT}`);
@@ -30,13 +34,18 @@ httpServer.listen(CONFIG.PORT, () => {
     });
 
     socket.on(EVENTS.CLIENT.CREATE_SESSION, async (data) => {
-      socket.data.host_id = data.host_id;
+      // As we switched from socketID to userID to identify hosts, it is no need for now
+      // socket.data.host_id = data.host_id;
+      
       const nanoid = customAlphabet(
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
         5
       );
       const sessionID = nanoid();
       socket.data.session = sessionID;
+      
+      // Saving to the maps of hostIDs with index based on sessionID
+      hostIDs[sessionID] = data.host_id;
 
       console.log(`${data.host_id} created room: ${sessionID}`);
       socket.join(sessionID);
@@ -60,6 +69,19 @@ httpServer.listen(CONFIG.PORT, () => {
 
       socket.emit(EVENTS.SERVER.JOIN_SESSION, {
         sessionID: data.sessionID,
+      });
+    });
+
+    socket.on(EVENTS.CLIENT.CHECK_IF_HOST, async (data) => {
+      const isHost = hostIDs[data.sessionID] === data.userID;
+    
+      if (DEBUG) {
+        console.log(`Checking if user ${data.userID} is host of session ${data.sessionID}`);
+        console.log(hostIDs[data.sessionID] + "(backend) | " + data.userID + " (input) =>" + isHost)
+      }
+
+      socket.emit(EVENTS.SERVER.CHECK_IF_HOST, {
+        isHost: isHost,
       });
     });
 
